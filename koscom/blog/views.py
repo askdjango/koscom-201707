60863a613831136aed94ff3adeaea7f01a45280b
Chinseok Lee <allieuslee@gmail.com>
2017-07-12 13:50:33 +0900

    인자 갯수와 상관없이 수행되는 mysum 구현

--- koscom/blog/views.py
+++ koscom/blog/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import render
 def post_list(request):
     return render(request, 'blog/post_list.html')
 
-def mysum(request, x, y, z):
-    return HttpResponse(int(x) + int(y) + int(z))
+def mysum(request, numbers):
+    # numbers = "10/20/30/40"
+    result = 0
+    for number in numbers.split('/'):  # ['10', '20', '30', '40']
+        result += int(number)
+    return HttpResponse(result)
 
