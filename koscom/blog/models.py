46136baebffca580bdea0063396f9ca2c86697cf
Chinseok Lee <allieuslee@gmail.com>
2017-07-12 15:42:10 +0900

    min_length 함수 구현

--- koscom/blog/models.py
+++ koscom/blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from .validators import min_length_3
+from .validators import min_length
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, validators=[min_length_3])
+    title = models.CharField(max_length=100, validators=[min_length(3)])
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- koscom/blog/validators.py
+++ koscom/blog/validators.py
@@ -1,5 +1,8 @@
 from django import forms
 
-def min_length_3(value):
-    if len(value) < 3:
-        raise forms.ValidationError('최소 3글자 이상 이벽해주세요.')
+def min_length(min_value):
+    def wrap(value):
+        if len(value) < min_value:
+            raise forms.ValidationError('최소 {}글자 이상 이벽해주세요.'.format(min_value))
+    return wrap
+
