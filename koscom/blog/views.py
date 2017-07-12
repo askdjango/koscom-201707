9201150b10e50b60174362561218cbbbbceb2701
Chinseok Lee <allieuslee@gmail.com>
2017-07-12 13:37:36 +0900

    mysum 인자 3개 처리

--- koscom/blog/views.py
+++ koscom/blog/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 def post_list(request):
     return render(request, 'blog/post_list.html')
 
-def mysum(request, x, y):
-    return HttpResponse(int(x) + int(y))
+def mysum(request, x, y, z):
+    return HttpResponse(int(x) + int(y) + int(z))
 
