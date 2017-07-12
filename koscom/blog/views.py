906baddbfeb3214d920dbecd988dc84a9282bebc
Chinseok Lee <allieuslee@gmail.com>
2017-07-12 13:33:03 +0900

    mysum 구현

--- koscom/blog/views.py
+++ koscom/blog/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def post_list(request):
     return render(request, 'blog/post_list.html')
+
+def mysum(request, x):
+    return HttpResponse(x)
 
