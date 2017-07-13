099dccb0ec4267d966cfbf1184ce5d8211d23d68
Chinseok Lee <allieuslee@gmail.com>
2017-07-13 09:44:19 +0900

    Post목록을 보여주는 post_list 뷰 구현

--- koscom/blog/views.py
+++ koscom/blog/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    qs = Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
 
 def mysum(request, numbers):
     # numbers = "10/20/30/40//"
