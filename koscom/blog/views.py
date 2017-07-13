36052989d196bf8fd4ac3ebbf02b31138b4d3c9b
Chinseok Lee <allieuslee@gmail.com>
2017-07-13 09:49:22 +0900

    blog.views.post_list 뷰 : 간단 검색 구현

--- koscom/blog/views.py
+++ koscom/blog/views.py
@@ -5,8 +5,14 @@ from .models import Post
 
 def post_list(request):
     qs = Post.objects.all()
+
+    query = request.GET.get('query', '')
+    if query:
+        qs = qs.filter(title__icontains=query)
+
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
+        'query': query,
     })
 
 def mysum(request, numbers):
