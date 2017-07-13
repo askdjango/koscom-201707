41cbd4c470d46f786ccb60385123c6aab78bb7aa
Chinseok Lee <allieuslee@gmail.com>
2017-07-13 09:18:49 +0900

    blog.Post 모델에 필수필드 author 추가

--- koscom/blog/models.py
+++ koscom/blog/models.py
@@ -3,6 +3,7 @@ from .validators import MinLengthValidator
 
 
 class Post(models.Model):
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
