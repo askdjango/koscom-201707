134b078ef4bd224f05a893dd2714325ab838bab6
Chinseok Lee <allieuslee@gmail.com>
2017-07-13 16:02:06 +0900

    PostSerializer를 통한 Post 생성

--- koscom/blog/api.py
+++ koscom/blog/api.py
@@ -1,8 +1,11 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Post
+from .serializers import PostSerializer
 
 
+@csrf_exempt
 def post_list(request):
     if request.method == 'GET':
         # 글목록
@@ -16,6 +19,9 @@ def post_list(request):
         return HttpResponse(json_string)
 
     elif request.method == 'POST':
-        # 받은 데이터로 글 생성
-        pass
+        serializer = PostSerializer(data=request.POST)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
