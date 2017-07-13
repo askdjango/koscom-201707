deb087b311845e8eacd03f8f7560b0bfeeaa8dd3
Chinseok Lee <allieuslee@gmail.com>
2017-07-13 09:13:08 +0900

    커스텀 validator를 클래스로 구현

--- koscom/blog/models.py
+++ koscom/blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from .validators import min_length
+from .validators import MinLengthValidator
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, validators=[min_length(3)])
+    title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- koscom/blog/validators.py
+++ koscom/blog/validators.py
@@ -1,8 +1,13 @@
 from django import forms
+from django.utils.deconstruct import deconstructible
 
-def min_length(min_value):
-    def wrap(value):
-        if len(value) < min_value:
-            raise forms.ValidationError('최소 {}글자 이상 이벽해주세요.'.format(min_value))
-    return wrap
+
+@deconstructible
+class MinLengthValidator:
+    def __init__(self, min_length):
+        self.min_length = min_length
+
+    def __call__(self, value):
+        if len(value) < self.min_length:
+            raise forms.ValidationError('최소 {}글자 이상 입력해주세요.'.format(self.min_length))
 
