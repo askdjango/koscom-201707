425c2a13cd31fff59bd82dad8b9a3c7f21660c43
Chinseok Lee <allieuslee@gmail.com>
2017-07-13 14:47:00 +0900

    blog.forms.PostForm 에서 title에 대한 clean_ 구현

--- koscom/blog/forms.py
+++ koscom/blog/forms.py
@@ -15,3 +15,9 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = '__all__' # 혹은 list
 
+    def clean_title(self):
+        title = self.cleaned_data['title']
+#       if len(title) < 10:
+#           raise forms.ValidationError('10글자 이상!!!')
+        return title.replace('_', '')  # 변환된 값으로 cleaned_data 에 저장
+
